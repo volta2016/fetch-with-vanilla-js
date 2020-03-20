77e9533a8df49de6ed29e05de886eafb998b17fb
volta <giovoltadesign@gmail.com>
2020-03-20 20:08:48 -0300

    agregando estilos y animación

--- fetch.js
+++ fetch.js
@@ -1,7 +1,15 @@
+let pokemon = document.querySelectorAll('#pokemon');
+let imagen = document.querySelectorAll('.modal__image');
+let pokename = document.querySelectorAll('modal__name');
+
+let close = document.querySelectorAll();
+let open = document.querySelectorAll();
+
 function showModal() {
-  document.getElementById('openModal').style.display = 'block';
+  document.getElementById('openModal').style.display = 'flex';
 }
 
 function closeModal() {
   document.getElementById('openModal').style.display = 'none';
 }
+
