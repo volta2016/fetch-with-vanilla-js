2655f8856134f7ec00f2976a81e9e37e5d0f3624
volta <giovoltadesign@gmail.com>
2020-03-21 19:38:27 -0300

    agregando funciones de modal, aplicando fetch para consumo de API - testing

--- fetch.js
+++ fetch.js
@@ -1,15 +1,53 @@
 let pokemon = document.querySelectorAll('#pokemon');
 let imagen = document.querySelectorAll('.modal__image');
-let pokename = document.querySelectorAll('modal__name');
+let pokename = document.querySelectorAll('.modal__name');
+let before = document.getElementById('before');
+let next = document.getElementById('next');
+let close = document.querySelectorAll('.modal');
 
-let close = document.querySelectorAll();
-let open = document.querySelectorAll();
 
 function showModal() {
   document.getElementById('openModal').style.display = 'flex';
 }
 
+
+
 function closeModal() {
-  document.getElementById('openModal').style.display = 'none';
+  document.getElementById('modal').style.display = 'none';
 }
+
+
+
+let start = 1;
+promesa(start)
+function promesa(start) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${start}`)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    renderImage(data.sprites.front_default);
+    pokename.innerHTML=`${(data.name).toUppercase()}`
+  })
+  .catch(() => {
+    renderImage('imagen.png')
+    pokename.innerHTML= ``
+  })
+}
+
+function renderImage(image) {
+  pokemon.setAttribute('src', image)
+}
+
+before.addEventListener('click', () => { 
+  start--;
+  promesa(start);
+})
+next.addEventListener('click', () => {
+  start++;
+  promesa(start);
+})
+
+
+
 
