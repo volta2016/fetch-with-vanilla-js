5a252d76e288c0d2d70959598607101c8cbcd4ae
volta <giovoltadesign@gmail.com>
2020-04-05 15:34:35 -0300

    iterando promesas finalizando fetch

--- fetch.js
+++ fetch.js
@@ -1,6 +1,6 @@
 let pokemon = document.getElementById('pokemon');
 let imagen = document.querySelectorAll('.modal__image');
-let pokename = document.querySelectorAll('.modal__name');
+let pokename = document.getElementById('name');
 let before = document.getElementById('before');
 let next = document.getElementById('next');
 let openmodal = document.getElementById('openModal');
@@ -17,35 +17,44 @@ function closeModal() {
   modal.style.animationName="modalOut"
 }
 
-
-let start = 1;
-promesa(start);
-function promesa(start) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${start}`)
+//si defino la var con el numero con del id del pokémon se refiere a que esta es la
+//data con la cual voy estar trabajando con las promesas por eso es "importante"
+//ver bien que estoy definiendo afuera o dentro de la promesa sobre todo 
+//si es una function, también ver lo que estoy retornando
+let data = 1;
+function promesa(data) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${data}`)
   .then(response => {
-    return response.json()
+    return response.json();
   })
   .then(data => {
     renderImage(data.sprites.front_default);
-    pokename.innerHTML=`${(data.name).toUppercase()}`
+    // renderName(data)
+    pokename.innerHTML = `${(data.name).toUpperCase()}`;
   })
   .catch(() => {
     renderImage('imagen.png')
     pokename.innerHTML= ``
   })
+  console.log(data);
+  console.log(data.name);
 }
+promesa(data);
 
 function renderImage(image) {
   pokemon.setAttribute("src", image);
 }
+// function renderName(data) {
+//   pokename.innerHTML = `${(data.name).toUpperCase()}`
+// }
 
 before.addEventListener("click", () => { 
-  start--;
-  promesa(start);
+  data--;
+  promesa(data);
 })
 next.addEventListener("click", () => {
-  start++;
-  promesa(start);
+  data++;
+  promesa(data);
 })
 
 
